f859d02f7d16a6afc4f475e21e4c14312996a87c
Yossi Spektor <easpex@gmail.com>
2021-02-26 10:34:17 +0200

    Add Grid.

---
+++ src/theme/Grid.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Card from './Card';
+
+const Grid = styled(Card)`
+  display: grid;
+  grid-template-columns: ${({ columns }: { columns: string }) =>
+    columns || '1fr 1fr 1fr 1fr'};
+`;
+
+export default Grid;
--- src/theme/index.js
+++ src/theme/index.js
@@ -2,3 +2,4 @@ export { default as Span } from './Span';
 export { default as Card } from './Card';
 export { default as Flex } from './Flex';
 export { default as Button } from './Button';
+export { default as Grid } from './Grid';
