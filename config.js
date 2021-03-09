0ab018364a3e87c54bdda1532fa46a62af693d3e
Yossi Spektor <easpex@gmail.com>
2021-03-09 22:38:40 +0200

    Delete duplicate config.

--- config.js
+++
@@ -1,6 +0,0 @@
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-export const websocketUrl = process.env.NEXT_PUBLIC_WS_URL;
-export const spotifyScopes = process.env.SPOTIFY_SCOPES;
-export const clientId = process.env.SPOTIFY_CLIENT_ID;
-export const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
-export const siteUrl = process.env.NEXT_PUBLIC_SITE;
